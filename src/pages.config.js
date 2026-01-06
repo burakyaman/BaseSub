cf132e454250f57788160557d6b7acc9bddd9fa6
Burak Yaman <burakyaman@gmail.com>
2026-01-06 17:17:31 +0000

    File changes

--- src/pages.config.js
+++ src/pages.config.js
@@ -3,6 +3,7 @@ import CalendarPage from './pages/CalendarPage';
 import Home from './pages/Home';
 import Insights from './pages/Insights';
 import Settings from './pages/Settings';
+import Onboarding from './pages/Onboarding';
 import __Layout from './Layout.jsx';
 
 
@@ -12,6 +13,7 @@ export const PAGES = {
     "Home": Home,
     "Insights": Insights,
     "Settings": Settings,
+    "Onboarding": Onboarding,
 }
 
 export const pagesConfig = {
