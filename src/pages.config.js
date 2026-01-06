d42f091f3bfb45cd4c38f670d74795b18270e9e4
Burak Yaman <burakyaman@gmail.com>
2026-01-06 13:10:36 +0000

    File changes

--- src/pages.config.js
+++ src/pages.config.js
@@ -1,17 +1,17 @@
-import CalendarPage from './pages/CalendarPage';
 import Analytics from './pages/Analytics';
-import Settings from './pages/Settings';
+import CalendarPage from './pages/CalendarPage';
 import Home from './pages/Home';
 import Insights from './pages/Insights';
+import Settings from './pages/Settings';
 import __Layout from './Layout.jsx';
 
 
 export const PAGES = {
-    "CalendarPage": CalendarPage,
     "Analytics": Analytics,
-    "Settings": Settings,
+    "CalendarPage": CalendarPage,
     "Home": Home,
     "Insights": Insights,
+    "Settings": Settings,
 }
 
 export const pagesConfig = {
